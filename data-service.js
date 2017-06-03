5692e5094b58c4eeaedc25f8398a2823ab8ca248
Stephen Ward <steve.ward2@gmail.com>
2017-06-02 23:03:44 -0400

    routing finished

--- data-service.js
+++ data-service.js
@@ -4,51 +4,142 @@ const fs = require('fs');
 var employees = [];
 var departments = [];
 
-module.exports.initialize = function(){
+module.exports.initialize = () => {
     var i = 0;
     var fail = false;
-    return new Promise(function(resolve, reject){
-        fs.readFile('./data/employees.json',(err, data) => {
-            if(err){
-                console.log("File could not be opened...");
+    return new Promise(function (resolve, reject) {
+        fs.readFile('./data/employees.json', (err, data) => {
+            if (err) {
                 fail = true;
-                
-            } else {  
-             employees = JSON.parse(data); 
-            }  
+            } else {
+                employees = JSON.parse(data);
+            }
         });
 
-        fs.readFile('./data/employees.json',(err, data) => {
-             if(err){
-                console.log("File could not be opened...");
+        fs.readFile('./data/departments.json', (err, data) => {
+            if (err) {
                 fail = true;
-                
-            } else {  
-             departments = JSON.parse(data); 
-            }  
+
+            } else {
+                departments = JSON.parse(data);
+            }
         });
 
-        if(fail){
-            reject("File could not be opened");
+        if (fail == true) {
+            reject("Did not open");
         } else {
             resolve();
         }
     });
 };
 
-module.exports.getAllEmployees = function() {
-    return new Promise(function (resolve, reject){
-        if (employees.length > 0){
+module.exports.getAllEmployees = () => {
+    return new Promise(function (resolve, reject) {
+        if (employees.length > 0) {
             resolve(employees);
-        } else{
+        } else {
             reject("No results returned");
-        }    
+        }
     });
 
 };
 
-module.exports.getEmployeesByStatus = function(status) {
+module.exports.getEmployeesByStatus = (status) => {
+    var statusTable = [];
+    return new Promise(function (resolve, reject) {
+        let t = 0;
+        for (let i = 0; i < employees.length; i++) {
+            if (employees[i].status == status) {
+                statusTable[t++] = employees[i];
+            }
+        }
 
-}
+        if (statusTable.length > 0) {
+            resolve(statusTable);
+        } else {
+            reject("No results returned");
+        }
+    });
+};
+
+module.exports.getEmployeesByDepartment = (department) => {
+    var departmentTable = [];
+    return new Promise(function (resolve, reject) {
+        let t = 0;
+        for (let i = 0; i < employees.length; i++) {
+            if (employees[i].department == department) {
+                departmentTable[t++] = employees[i];
+            }
+        }
+
+        if (departmentTable.length > 0) {
+            resolve(departmentTable);
+        } else {
+            reject("No results returned");
+        }
+    });
+};
+
+module.exports.getEmployeesByManager = (managerId) => {
+    var managerTable = [];
+    return new Promise(function (resolve, reject) {
+        let t = 0;
+        for (let i = 0; i < employees.length; i++) {
+            if (employees[i].employeeManagerNum == managerId) {
+                managerTable[t++] = employees[i];
+            }
+        }
+
+        if (managerTable.length > 0) {
+            resolve(managerTable);
+        } else {
+            reject("No results returned");
+        }
+    });
+};
+
+module.exports.getEmployeeByNum = (num) => {
+    var flag = 0;
+    return new Promise(function (resolve, reject) {
+        for (let i = 0; i < employees.length; i++){
+            if (employees[i].employeeNum == num){
+                resolve(employees[i]);
+                flag = 1;
+            } 
+        }
+        if (flag == 0){
+            reject("No employee found");
+        }
+    });
+};
+
+module.exports.getManagers = () => {
+    var managers = [];
+    let t = 0;
+    return new Promise(function (resolve, reject){
+        for (let i = 0; i < employees.length; i++){
+            if (employees[i].isManager == true){
+                managers[t++] = employees[i];
+            }
+        }
+        if (managers.length > 0){
+            resolve(managers);
+        } else {
+            reject("No results found");
+        }
+    });
+};
+
+module.exports.getDepartments =  () =>{
+return new Promise(function (resolve, reject) {
+        if (departments.length > 0) {
+            resolve(departments);
+        } else {
+            reject("No results returned");
+        }
+    });
+
+};
+
 
 
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part
 *  of this assignment has been copied manually or electronically from any other source
 *  (including 3rd party web sites) or distributed to other students.
 *
-*  Name: Stephen Ward Student ID: 118862168 Date: 05/26/2017
+*  Name: Stephen Ward Student ID: 118862168 Date: 06/02/2017
 *  Online (Heroku) Link:  https://intense-everglades-43152.herokuapp.com/
 *
 ********************************************************************************/
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 
 app.get("/", (req, res) => {
- res.sendFile(path.join(__dirname, './views', 'home.html'));
+  res.sendFile(path.join(__dirname, './views', 'home.html'));
 });
 
 app.get("/about", (req, res) => {
@@ -27,50 +27,57 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/employees", (req, res) => {
- 
-  if(req.query.status){
-    res.send(req.query.status);
 
-  }else if(req.query.department){
-    res.send(req.query.department);
+  if (req.query.status) {
+    dataModule.getEmployeesByStatus(req.query.status).then((data) => {
+      res.json(data);
+    });
 
-  }else if (req.query.manager){
-    res.send(req.query.manager);
-  }else {
-   dataModule.getAllEmployees().then((data) => {
-     res.json(data);
-   });
+  } else if (req.query.department) {
+    dataModule.getEmployeesByDepartment(req.query.department).then((data) => {
+      res.json(data);
+    });
+
+  } else if (req.query.manager) {
+    dataModule.getEmployeesByManager(req.query.manager).then((data) => {
+      res.json(data);
+    });
+    //if there are no options, display all the employees
+  } else {
+    dataModule.getAllEmployees().then((data) => {
+      res.json(data);
+    });
   }
 });
 
-app.get("employee/:empNum", (req, res) => {
-
-
-res.json(req.params.empNum);
-
+app.get('/employee/:empNum', (req, res) => {
+  dataModule.getEmployeeByNum(req.params.empNum).then((data) => {
+    res.json(data);
+  });
 });
 
 app.get("/managers", (req, res) => {
-  
-  res.send("managers");
-
+  dataModule.getManagers().then((data) => {
+    res.json(data);
+  });
 });
 
-app.get("/departments", (req,res) => {
-
-res.send("departments");
+app.get("/departments", (req, res) => {
+dataModule.getDepartments().then((data) => {
+    res.json(data);
+  });
 });
 
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
   res.status(404).send("Page Not Found");
-  
+
 });
 
-dataModule.initialize().then( () =>{
-  app.listen(HTTP_PORT,(req, res) => {
+dataModule.initialize().then(() => {
+  app.listen(HTTP_PORT, (req, res) => {
     console.log("Express http server listing on " + HTTP_PORT);
   });
 }).catch((err) => {
-    res.send(err);
+  res.send(err);
 });
 
